8623316bc6bbacaae28bf4b6e16caf573b298206
KForster99 <68499784+KForster99@users.noreply.github.com>
2021-09-29 00:22:22 +0700

    DB_CONNECT

---
+++ connection_connect.php
@@ -0,0 +1,11 @@
+<?php
+    $servername = "localhost";
+    $username = "db21_024";
+    $password = "db21_024";
+    $dbname = "db21_024";
+    $conn = new mysqli($servername,$username,$password);
+    if($conn->connect_error)
+        die("Connection failed: ".$conn->connect_error);
+    if(!$conn->select_db($dbname))
+        die("Connection failed: ".$conn->connect_error);
+    
