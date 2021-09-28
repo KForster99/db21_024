e69127d03db1bf2792029bdfb166ad4f8c3698dd
KForster99 <68499784+KForster99@users.noreply.github.com>
2021-09-28 13:16:03 +0700

    Connect DB

--- Page1.php
+++ Page1.php
@@ -3,7 +3,16 @@
 <body>
 
     <?php
-    echo "My first PHP script!";
+    $servername = "localhost";
+    $username = "db21_024";
+    $password = "db21_024";
+
+    $conn = new mysqli($servername, $username, $password);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
     ?>
 
 </body>
