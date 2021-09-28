3c9e26da8913624271b53059fa9389b0fa7507be
KForster99 <68499784+KForster99@users.noreply.github.com>
2021-09-28 22:17:48 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,7 +1,11 @@
 <?php
 $controllers = array('pages'=>['home','error']);
 function call($controller,$action){
-    echo "routes to ".$controller."-".$action."<br>";
+    require_once("controllers/".$controller."_controller.php");
+    switch($controller){
+        case "pages": $controller = new PagesController();
+                      break;
+    }$controller->{$action}();
 }
 if(array_key_exists($controller,$controllers)){
     if(in_array($action,$controllers[$controller]))
