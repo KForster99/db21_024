88abbe1156ca0b545e6638ee5f23007b37beb0b7
ThanachoteI <91516544+ThanachoteI@users.noreply.github.com>
2021-09-30 03:01:30 +0700

    Update index_productRate.php

--- views/productRate/index_productRate.php
+++ views/productRate/index_productRate.php
@@ -8,14 +8,14 @@
 <td> update </td>
 <td> delete </td>
 </tr>
-<?php foreach( $quotation_list as $quotation ) {
-    echo "<tr> 
-    <td> $quotation->Q_ID </td>
-    <td> $quotation->date </td> 
-    <td> $quotation->cusName </td>
-    <td> $quotation->cusAddress </td>
-    <td> $quotation->cusPhone </td> 
-    <td> $quotation->empName </td> 
+<?php foreach( $productRate_list as $productRate ) {
+    echo "<tr>
+    <td> $productRate->P_ID </td> 
+    <td> $productRate->P_Name </td>
+    <td> $productRate->QtyMorethan </td>
+    <td> $productRate->cusAddress </td>
+    <td> $productRate->Price </td> 
+    <td> $productRate->ScreenPrice </td> 
     <td> update </td>
     <td> delete </td> </tr>"; 
 }
