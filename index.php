ea41f7bc6a39727563267caab6d30e02d133efbc
ThanachoteI <91516544+ThanachoteI@users.noreply.github.com>
2021-09-30 03:03:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ if(isset($_GET['controller'])&&isset($_GET['action']))
     <br>[<a href="?controller=pages&action=home">Home </a>] 
         [<a href="?controller=quotation&action=index">quotation</a>]
         [<a href="?controller=quotationDetail&action=index">quotationDetail</a>]
+        [<a href="?controller=productRate&action=index">productRate</a>]
     <br>
 
    
