22bc4b6566236233e564a09ff8d3e2d6854d96c5
ThanachoteI <91516544+ThanachoteI@users.noreply.github.com>
2021-09-30 02:21:12 +0700

    Create productModel.php

---
+++ models/productModel.php
@@ -0,0 +1,26 @@
+<?php
+class Product
+{
+    public $proID, $proName;
+
+    public function __construct($proID, $proName)
+    {
+        $this->proID = $proID;
+        $this->proName = $proName;
+    }
+    public static function getAll()
+    {
+        echo "getAll";
+        $productList = [];
+        require("connection_connect.php");
+        $sql = "select * from product";
+        $result = $conn->query($sql);
+        while ($my_row = $result->fetch_assoc()) {
+            $proID = $my_row[P_ID];
+            $proName = $my_row[P_Name];
+            $productList[] = new ProductColor($proID, $proName);
+        }
+        require("connection_close.php");
+        return $productList;
+    }
+}
