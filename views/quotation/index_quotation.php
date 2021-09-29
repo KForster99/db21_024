32c1e3779f776ef63ae0ea26a4edb64907efc8c6
NuttawatJuntawong <91516482+NuttawatJuntawong@users.noreply.github.com>
2021-09-29 14:42:19 +0700

    Update index_quotation.php

--- views/quotation/index_quotation.php
+++ views/quotation/index_quotation.php
@@ -2,10 +2,10 @@
 <table border = 1>
 <tr> <td>ID</td><td>CusName</td><td>EmpName</td>
 <td>update</td><td>delete</td> </tr>
-<?php foreach($quotation_list as $quotation){
+<?php foreach($quotation_list as $qt){
     echo "<tr> <td>$quotation->id</td>
-    <td>$quotation->cusName</td>  
-    <td>$quotation->empName</td> 
+    <td>$qt->cusName</td>  
+    <td>$qt->empName</td> 
     <td> update</td>
     <td>delete</td> </tr>"; }
 echo "</table>";
