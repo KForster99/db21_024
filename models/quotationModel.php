32c25705dba1fea1a613604fa9901e2da754a05b
KForster99 <68499784+KForster99@users.noreply.github.com>
2021-09-29 23:07:01 +0700

    Quotation head

--- models/quotationModel.php
+++ models/quotationModel.php
@@ -2,13 +2,19 @@
 class Quotation
 {
     public $id;
+    public $date;
     public $cusName;
+    public $cusAddress;
+    public $cusPhone;
     public $empName;
 
-    public function __construct($id, $cusName, $empName)
+    public function __construct($id, $date, $cusName, $cusAddress, $cusPhone, $empName)
     {
         $this->id = $id;
+        $this->date = $date;
         $this->cusName = $cusName;
+        $this->cusAddress = $cusAddress;
+        $this->cusPhone = $cusPhone;
         $this->empName = $empName;
     }
     // public static function get()
@@ -29,9 +35,12 @@ class Quotation
         $result = $conn->query($sql);
         while ($my_row = $result->fetch_assoc()) {
             $id = $my_row[Q_ID];
+            $date = $my_row[Q_Date];
             $cusName = $my_row[C_Name];
+            $cusAddress = $my_row[C_Address];
+            $cusPhone = $my_row[C_Phone];
             $empName = $my_row[E_Name];
-            $quotationList[] = new Quotation($id, $cusName, $empName);
+            $quotationList[] = new Quotation($id, $date, $cusName, $cusAddress, $cusPhone, $empName);
         }
         require("connection_close.php");
         return $quotationList;
--- views/quotation/index_quotation.php
+++ views/quotation/index_quotation.php
@@ -1,12 +1,22 @@
-
 <table border = 1>
 new quotation<a href="?controller=quotation&action=newQuotation"> click </a><br>
-<tr> <td>ID</td><td>cusName</td><td>empName</td>
-<td>update</td><td>delete</td> </tr>
+<tr>
+<td>ID</td>
+<td>Date</td>
+<td>cusName</td>
+<td>cusAddress</td>
+<td>cusPhone</td>
+<td>empName</td>
+<td>update</td>
+<td>delete</td>
+</tr>
 <?php foreach($quotation_list as $quotation){
     echo "<tr> 
     <td>$quotation->id</td>
-    <td>$quotation->cusName</td>  
+    <td>$quotation->date</td> 
+    <td>$quotation->cusName</td>
+    <td>$quotation->cusAddress</td>
+    <td>$quotation->cusPhone</td> 
     <td>$quotation->empName</td> 
     <td> update</td>
     <td>delete</td> </tr>"; }
