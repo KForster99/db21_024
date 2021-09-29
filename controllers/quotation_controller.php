d1bc45c890152246f259b7760cd7e914a09569e0
NuttawatJuntawong <91516482+NuttawatJuntawong@users.noreply.github.com>
2021-09-29 14:40:53 +0700

    Update quotation_controller.php

--- controllers/quotation_controller.php
+++ controllers/quotation_controller.php
@@ -2,7 +2,7 @@
     class QuotationController{
         public function index()
         {
-            $quotation_list[] = Quotation::getAll();
+            $quotation_list = Quotation::getAll();
             require_once("./views/quotation/index_quotation.php");
         }
     }
