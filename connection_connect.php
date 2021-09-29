526d869c0251f5a4966cad6a8ddb44ab16bb9010
NuttawatJuntawong <91516482+NuttawatJuntawong@users.noreply.github.com>
2021-09-29 14:57:49 +0700

    Update connection_connect.php

--- connection_connect.php
+++ connection_connect.php
@@ -4,10 +4,16 @@
     $password = "db21_024";
     $dbname = "db21_024";
     $conn = new mysqli($servername,$username,$password);
-    if($conn->connect_error)
+    if($conn->connect_error){
         die("Connection failed: ".$conn->connect_error);
-    if(!$conn->select_db($dbname))
+        echo "Fail connected to database <br>";
+    }
+        
+    if(!$conn->select_db($dbname)){
         die("Connection failed: ".$conn->connect_error);
+        echo "Fail select to database <br>";
+    }
+        
     else{
         echo "Successfully connected to database <br>";
     }
