ad91e551d2a5ca2ed39aba876733fde12fa48265
ThanachoteI <91516544+ThanachoteI@users.noreply.github.com>
2021-09-30 16:22:07 +0700

    Update productModel.php

--- models/productModel.php
+++ models/productModel.php
@@ -11,19 +11,22 @@
         }
 
         public static function getAll(){
-            $productList = [];
-            echo "getAll";
-            require("connection_connect.php");
-            $sql = "select * from product";
+            $quotationDetailList = [];
+            require( "connection_connect.php" );
+            $sql = "select * from product,product_rate where product.P_ID = product_rate.P_ID";
             $result = $conn->query($sql);
-            echo "getAll2";
-            while ($my_row = $result->fetch_assoc()) {
-                $ID = $my_row[P_ID];
-                $Name = $my_row[P_Name];
-                $productList[] = new ProductColor($ID, $Name);
-            } 
+            while ( $my_row = $result->fetch_assoc() ) {
+                $ID = $my_row[PR_ID];
+                $P_ID = $my_row[P_ID];
+                $P_Name = $my_row[P_Name];
+                $QtyMoreThan = $my_row[PR_QtyMoreThan];
+                $Price = $my_row[PR_Price];
+                $ScreenPrice = $my_row[PR_ScreenPrice];
+
+                $productRateList[] = new ProductRate($ID ,$P_ID ,$P_Name ,$QtyMoreThan ,$Price ,$ScreenPrice);
+            }
             require("connection_close.php");
-            return $productList;
+            return $productRateList;
         }
 
     }
