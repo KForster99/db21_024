e16fd7ba445da0be6b1b5c10d882d2d3ae5ccd68
ThanachoteI <91516544+ThanachoteI@users.noreply.github.com>
2021-09-30 16:24:10 +0700

    Update productModel.php

--- models/productModel.php
+++ models/productModel.php
@@ -11,19 +11,14 @@
         }
 
         public static function getAll(){
-            $quotationDetailList = [];
+            $productRateList = [];
             require( "connection_connect.php" );
-            $sql = "select * from product,product_rate where product.P_ID = product_rate.P_ID";
+            $sql = "select * from product";
             $result = $conn->query($sql);
             while ( $my_row = $result->fetch_assoc() ) {
-                $ID = $my_row[PR_ID];
-                $P_ID = $my_row[P_ID];
-                $P_Name = $my_row[P_Name];
-                $QtyMoreThan = $my_row[PR_QtyMoreThan];
-                $Price = $my_row[PR_Price];
-                $ScreenPrice = $my_row[PR_ScreenPrice];
-
-                $productRateList[] = new ProductRate($ID ,$P_ID ,$P_Name ,$QtyMoreThan ,$Price ,$ScreenPrice);
+                $ID = $my_row[P_ID];
+                $Name = $my_row[P_Name];
+                $productRateList[] = new ProductRate($ID ,$Name);
             }
             require("connection_close.php");
             return $productRateList;
