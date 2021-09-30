465424b7bdb266d413fa767c952b8686f3af0eba
ThanachoteI <91516544+ThanachoteI@users.noreply.github.com>
2021-09-30 14:56:42 +0700

    Update productRate_controller.php

--- controllers/productRate_controller.php
+++ controllers/productRate_controller.php
@@ -1,9 +1,18 @@
 <?php
+
     class ProductRateController{
-        public function index()
-        {
+
+        public function index(){
             $productRate_list = ProductRate::getAll();
             require_once("./views/productRate/index_productRate.php");
         }
+
+        public function newQuotation(){
+            $customer_list = Customer::getAll();
+            $employee_list = Employee::getAll();
+            require_once("./views/quotation/newQuotation.php");
+        }
+
     }
+    
 ?>
