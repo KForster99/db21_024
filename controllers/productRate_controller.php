ca97e399b3f7c81a83d8537e7c6414efde247560
ThanachoteI <91516544+ThanachoteI@users.noreply.github.com>
2021-09-30 23:36:49 +0700

    Update productRate_controller.php

--- controllers/productRate_controller.php
+++ controllers/productRate_controller.php
@@ -38,7 +38,7 @@
             $ID = $_GET['ID'];
             $productRate = ProductRate::get($ID);
             $product_list = Product::getAll();
-            require_once("./views/pages/home.php");
+            require_once("./views/productRate/updateFrom.php");
         }
 
     }
