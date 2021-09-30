109f69f7ed6165f6a58d30cdad0fb2ec421d3579
ThanachoteI <91516544+ThanachoteI@users.noreply.github.com>
2021-09-30 15:39:06 +0700

    Update newProductRate.php

--- views/productRate/newProductRate.php
+++ views/productRate/newProductRate.php
@@ -1,11 +1,5 @@
 <form method="get" action="">
 
-<label>สินค้า <select name="P_ID">
-<?php foreach($product_list as $product){
-        echo "<option value = $product->ID>
-        $product->Name</option>";}?>
-</select></label><br>
-
 <label>จำนวนมากกว่า <input type="text" name="QtyMoreThan"/> </label><br>
 <label>ราคา <input type="text" name="Price"/> </label><br>
 <label>ราคาสกรีน <input type="text" name="ScreenPrice"/> </label><br>
