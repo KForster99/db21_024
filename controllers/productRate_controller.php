14f313915596602efbf1d4d8957276a3662631d7
ThanachoteI <91516544+ThanachoteI@users.noreply.github.com>
2021-09-30 17:41:54 +0700

    Update productRate_controller.php

--- controllers/productRate_controller.php
+++ controllers/productRate_controller.php
@@ -12,6 +12,21 @@
             require_once("./views/productRate/newProductRate.php");
         }
 
+        public function addQuotationDetail()
+        {
+            $QID = $_GET['QID'];
+            $PID = $_GET['proID'];
+            $Color = $_GET['PColor'];
+            $Unit = $_GET['Unit'];
+            $UnitPrice = $_GET['UnitPrice'];
+            $Amount = $_GET['Amount'];
+            
+            echo $QID." ".$PID." ".$Color." ".$Unit." ".$UnitPrice." ".$Amount;
+
+            QuotationDetail::Add($QID,$PID,$Color,$Unit,$UnitPrice,$Amount);
+            QuotationDetailController::index();
+        }
+
     }
 
 ?>
