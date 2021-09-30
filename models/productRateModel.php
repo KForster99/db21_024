0d68afff903e4f3d0576db15fc4c30ed8ac698d2
ThanachoteI <91516544+ThanachoteI@users.noreply.github.com>
2021-09-30 21:41:46 +0700

    Update productRateModel.php

--- models/productRateModel.php
+++ models/productRateModel.php
@@ -18,16 +18,24 @@
             $this->ScreenPrice = $ScreenPrice;
         }
 
-        // public static function get()
-        // {
-        //     require("connection_connect.php");
-        //     $sql = "";
-        //     $result = $conn->query($sql);
-        //     $my_row = $result->fetch_assoc();
+        public static function get($ID){
+            $productRateList = [];
+            require("connection_connect.php");
+            $sql = "SELECT * FROM product,product_rate WHERE product.P_ID = product_rate.P_ID AND product_rate.PR_ID = '$ID'";
+            $result = $conn->query($sql);
+            while ($my_row = $result->fetch_assoc()) {
+                $ID = $my_row[PR_ID];
+                $P_ID = $my_row[P_ID];
+                $P_Name = $my_row[P_Name];
+                $QtyMoreThan = $my_row[PR_QtyMoreThan];
+                $Price = $my_row[PR_Price];
+                $ScreenPrice = $my_row[PR_ScreenPrice];
 
-        //     require("connection_close.php");
-        //     return ;
-        // }
+                $productRateList[] = new ProductRate($ID, $P_ID, $P_Name, $QtyMoreThan, $Price, $ScreenPrice);
+            }
+            require("connection_close.php");
+            return $productRateList;
+        }
 
         public static function getAll(){
             $productRateList = [];
@@ -54,7 +62,7 @@
             require("connection_connect.php");
             $sql = "SELECT * FROM product,product_rate WHERE product.P_ID = product_rate.P_ID AND ( product.P_ID LIKE '%$key%' OR product.P_Name LIKE '%$key%')";
             $result = $conn->query($sql);
-            while ($my_row = $result->fetch_assoc()) {
+            while ($my_row = $result->fetch_assoc()) {  
                 $ID = $my_row[PR_ID];
                 $P_ID = $my_row[P_ID];
                 $P_Name = $my_row[P_Name];
