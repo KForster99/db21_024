c8203c5196482120829c085fc15b369300ae9a0f
ThanachoteI <91516544+ThanachoteI@users.noreply.github.com>
2021-09-30 16:11:12 +0700

    Update newProductRate.php

--- views/productRate/newProductRate.php
+++ views/productRate/newProductRate.php
@@ -1,1 +1,11 @@
-<p> Welcome to </p>
+<form method="get" action="">
+
+<label>จำนวนมากกว่า <input type="text" name="QtyMoreThan"/> </label><br>
+<label>ราคา <input type="text" name="Price"/> </label><br>
+<label>ราคาสกรีน <input type="text" name="ScreenPrice"/> </label><br>
+
+<input type="hidden" name="controller" value="productRate"/>
+<button type="submit" name="action" value="index"> Back</button>
+<button type="submit" name="action" value="addProductRate"> Save</button>
+
+</form>
