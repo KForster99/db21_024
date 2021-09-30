a9459b65f8e717cac2dac81a4d37d73fb745b5ff
ThanachoteI <91516544+ThanachoteI@users.noreply.github.com>
2021-09-30 14:57:38 +0700

    Update productRateModel.php

--- models/productRateModel.php
+++ models/productRateModel.php
@@ -1,86 +1,90 @@
 <?php
-class ProductRate
-{
-    public $ID;
-    public $P_ID;
-    public $P_Name;
-    public $QtyMorethan;
-    public $Price;
-    public $ScreenPrice;
 
+    class ProductRate{
 
-    public function __construct($ID ,$P_ID ,$P_Name ,$QtyMorethan ,$Price ,$ScreenPrice)
-    {
-        $this->ID = $ID;
-        $this->P_ID = $P_ID;
-        $this->P_Name = $P_Name;
-        $this->QtyMorethan = $QtyMorethan;
-        $this->Price = $Price;
-        $this->ScreenPrice = $ScreenPrice;
+        public $ID;
+        public $P_ID;
+        public $P_Name;
+        public $QtyMorethan;
+        public $Price;
+        public $ScreenPrice;
 
-    }
-    // public static function get()
-    // {
-    //     require("connection_connect.php");
-    //     $sql = "";
-    //     $result = $conn->query($sql);
-    //     $my_row = $result->fetch_assoc();
-
-    //     require("connection_close.php");
-    //     return ;
-    // }
-    public static function getAll()
-    {
-        $quotationDetailList = [];
-        require( "connection_connect.php" );
-        $sql = "select * from product,product_rate where product.P_ID = product_rate.P_ID";
-        $result = $conn->query($sql);
-        while ( $my_row = $result->fetch_assoc() ) {
-            $ID = $my_row[PR_ID];
-            $P_ID = $my_row[P_ID];
-            $P_Name = $my_row[P_Name];
-            $QtyMorethan = $my_row[PR_QtyMoreThan];
-            $Price = $my_row[PR_Price];
-            $ScreenPrice = $my_row[PR_ScreenPrice];
-
-            $productRateList[] = new ProductRate($ID ,$P_ID ,$P_Name ,$QtyMorethan ,$Price ,$ScreenPrice);
+        public function __construct($ID ,$P_ID ,$P_Name ,$QtyMorethan ,$Price ,$ScreenPrice){
+            $this->ID = $ID;
+            $this->P_ID = $P_ID;
+            $this->P_Name = $P_Name;
+            $this->QtyMorethan = $QtyMorethan;
+            $this->Price = $Price;
+            $this->ScreenPrice = $ScreenPrice;
         }
-        require("connection_close.php");
-        return $productRateList;
-    }
-    // public static function search()
-    // {
-    //     require("connection_connect.php");
-    //     $sql = "";
-    //     $result = $conn->query($sql);
-    //     while ($my_row = $result->fetch_assoc()) {
 
-    //     }
-    //     require("connection_close.php");
-    //     return ;
-    // }
-    // public static function Add()
-    // {
-    //     require("connection_connect.php");
-    //     $sql = "";
-    //     $result = $conn->query($sql);
-    //     require("connection_close.php");
-    //     return ;
-    // }
-    // public static function update()
-    // {
-    //     require("connection_connect.php");
-    //     $sql = "";
-    //     $result = $conn->query($sql);
-    //     require("connection_close.php");
-    //     return ;
-    // }
-    // public static function delete()
-    // {
-    //     require_once("connection_connect.php");
-    //     $sql = "";
-    //     $result = $conn->query($sql);
-    //     require("connection_close.php");
-    //     return ;
-    // }
+        // public static function get()
+        // {
+        //     require("connection_connect.php");
+        //     $sql = "";
+        //     $result = $conn->query($sql);
+        //     $my_row = $result->fetch_assoc();
+
+        //     require("connection_close.php");
+        //     return ;
+        // }
+
+        public static function getAll(){
+            $quotationDetailList = [];
+            require( "connection_connect.php" );
+            $sql = "select * from product,product_rate where product.P_ID = product_rate.P_ID";
+            $result = $conn->query($sql);
+            while ( $my_row = $result->fetch_assoc() ) {
+                $ID = $my_row[PR_ID];
+                $P_ID = $my_row[P_ID];
+                $P_Name = $my_row[P_Name];
+                $QtyMorethan = $my_row[PR_QtyMoreThan];
+                $Price = $my_row[PR_Price];
+                $ScreenPrice = $my_row[PR_ScreenPrice];
+
+                $productRateList[] = new ProductRate($ID ,$P_ID ,$P_Name ,$QtyMorethan ,$Price ,$ScreenPrice);
+            }
+            require("connection_close.php");
+            return $productRateList;
+        }
+
+        // public static function search()
+        // {
+        //     require("connection_connect.php");
+        //     $sql = "";
+        //     $result = $conn->query($sql);
+        //     while ($my_row = $result->fetch_assoc()) {
+
+        //     }
+        //     require("connection_close.php");
+        //     return ;
+        // }
+
+        // public static function Add()
+        // {
+        //     require("connection_connect.php");
+        //     $sql = "";
+        //     $result = $conn->query($sql);
+        //     require("connection_close.php");
+        //     return ;
+        // }
+
+        // public static function update()
+        // {
+        //     require("connection_connect.php");
+        //     $sql = "";
+        //     $result = $conn->query($sql);
+        //     require("connection_close.php");
+        //     return ;
+        // }
+
+        // public static function delete()
+        // {
+        //     require_once("connection_connect.php");
+        //     $sql = "";
+        //     $result = $conn->query($sql);
+        //     require("connection_close.php");
+        //     return ;
+        // }
+
 }
