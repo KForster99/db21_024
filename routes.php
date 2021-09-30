f31a40849049451f87727c68087db9b0749974b3
ThanachoteI <91516544+ThanachoteI@users.noreply.github.com>
2021-09-30 17:41:51 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -2,7 +2,7 @@
 $controllers = array('pages'=>['home','error'],
                     'quotation'=>['index','newQuotation'],
                     'quotationDetail'=>['index','newQuotationDetail','addQuotationDetail'],
-                    'productRate'=>['index','newProductRate']);
+                    'productRate'=>['index','newProductRate','addProductRate']);
 
 function call($controller,$action){
     require_once("./controllers/".$controller."_controller.php");
