a857bb3f814e4d8738a4e3d798c74b856aa40fd5
ThanachoteI <91516544+ThanachoteI@users.noreply.github.com>
2021-09-30 14:57:40 +0700

    Update productModel.php

--- models/productModel.php
+++ models/productModel.php
@@ -1,27 +1,27 @@
 <?php
-class Product
-{
-    public $proID;
-    public $proName;
 
-    public function __construct($proID, $proName)
-    {
-        $this->proID = $proID;
-        $this->proName = $proName;
-    }
-    public static function getAll()
-    {
-        echo "getAll";
-        $productList = [];
-        require("connection_connect.php");
-        $sql = "select * from product";
-        $result = $conn->query($sql);
-        while ($my_row = $result->fetch_assoc()) {
-            $proID = $my_row[P_ID];
-            $proName = $my_row[P_Name];
-            $productList[] = new ProductColor($proID, $proName);
+    class Product{
+
+        public $proID;
+        public $proName;
+
+        public function __construct($proID, $proName){
+            $this->proID = $proID;
+            $this->proName = $proName;
         }
-        require("connection_close.php");
-        return $productList;
+
+        public static function getAll(){
+            echo "getAll";
+            $productList = [];
+            require("connection_connect.php");
+            $sql = "select * from product";
+            $result = $conn->query($sql);
+            while ($my_row = $result->fetch_assoc()) {
+                $proID = $my_row[P_ID];
+                $proName = $my_row[P_Name];
+                $productList[] = new ProductColor($proID, $proName);
+            }
+            require("connection_close.php");
+            return $productList;
+        }
     }
-}
