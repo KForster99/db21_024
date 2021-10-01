db811a206fbf07307c9b800aa7ce9261f43da752
KForster99 <68499784+KForster99@users.noreply.github.com>
2021-10-01 17:11:54 +0700

    update

--- routes.php
+++ routes.php
@@ -1,6 +1,6 @@
 <?php
 $controllers = array('pages'=>['home','error'],
-                    'quotation'=>['index','newQuotation','searchQuotation','addQuotation'],
+                    'quotation'=>['index','newQuotation','searchQuotation','addQuotation','updateFormQuotation', 'updateQuotation'],
                     'quotationDetail'=>['index','newQuotationDetail','addQuotationDetail','searchQuotationDetail', 'updateFormQuotationDetail', 'updateQuotationDetail'],
                     'productRate'=>['index', 'newProductRate', 'addProductRate', 'search', 'updateForm', 'update', 'deleteConfirm', 'delete']);
 
---
+++ views/quotation/updateFromQuotation.php
@@ -0,0 +1,30 @@
+<form method="get" action="">
+    <label>เลขใบเสนอราคา&nbsp;&nbsp;&nbsp;&nbsp;
+        <select name="QID">
+            <?php
+            foreach ($quotation_list as $quotation) {
+                echo "<option value = $quotation->Q_ID>$quotation->Q_ID</option>";
+            } ?></select></label><br>
+    <label>วันที่&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input type="date" name="date" /></label><br>
+    <label>ลูกค้า&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <select name="cusID">
+            <?php
+            foreach ($quotation_list as $quotation) {
+                echo "<option value=$quotation->C_ID>$quotation->cusName</option>";
+            } ?> </select></label><br>
+    <label>ที่อยู่ลูกค้า&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input type="text" name="cusAddress" /></label><br>
+    <label>เบอร์โทรศัพท์&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input type="text" name="cusPhone" /></label><br>
+    <label>พนักงาน&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <select name="empID">
+            <?php
+            foreach ($quotation_list as $quotation) {
+                echo "<option value=$quotation->E_ID>$quotation->empName</option>";
+            } ?></select></label><br>
+
+    <input type="hidden" name="controller" value="quotation" />
+    <button type="submit" name="action" value="index"> Back</button>
+    <button type="submit" name="action" value="addQuotation"> Save</button>
+</form>
